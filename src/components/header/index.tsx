b1c06accd93f841f9b3f78da38aaa854f358111a
Ariane de Almeida Franchetto <nanifranchetto@gmail.com>
2022-05-13 22:24:44 -0300

    header and css global
    add header
    add global css

---
+++ src/components/header/index.tsx
@@ -0,0 +1,12 @@
+
+import logoImg from '../../asstes/logo.svg'
+
+
+export function Header() {
+    return (
+      <header> 
+       <img src="{logoImg}" alt="dt money"/>
+       <button>Nova Transação</button>
+      </header>
+    )
+}

1f4438ead9a3db2e0acfef16553ab6e4213cf727
Ariane de Almeida Franchetto <nanifranchetto@gmail.com>
2022-05-23 23:17:16 -0300

    axios and miragejs
    add axios and miragejs

--- src/components/TransactionTable/index.tsx
+++ src/components/TransactionTable/index.tsx
@@ -1,6 +1,19 @@
+import { useEffect } from 'react'
 import '../../styles/transiction.css'
+import { api } from '../services/api'
 
 export function TransactionTable () {
+
+    useEffect(() => {
+        api.get('/transactions')
+        .then(response =>  (response.data))
+    }
+    
+    
+    
+    )
+
+
     return (
         <div className="dash-container">
            <thead>
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,13 +1,27 @@
 import '../../styles/header.css'
 import logoImg from '../../assets/logo.svg'
+import { useState } from 'react';
 
 export function Header() {
+
+const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState (false);
+
+function handleOpenNewTransactionModal() {
+  setIsNewTransactionModalOpen(true)
+}
+
+function handleCloseNewTransactionModal() {
+  setIsNewTransactionModalOpen(false)
+
+}
+
+
     return (
       <header className='container'> 
         
        <div className='content'> 
        <img src= {logoImg} alt="dt money"/>
-       <button type="button">Nova Transação</button>
+       <button type="button" onClick={handleOpenNewTransactionModal} >Nova Transação</button>
        </div>
       
        
